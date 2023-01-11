5d17257144044a7e2097bcb8737c78e6f33d4fc2
jisu <45481968+sgmsgood@users.noreply.github.com>
2023-01-11 10:26:07 +0900

    add dropdown box

--- lib/modules/timer/page_timer.dart
+++ lib/modules/timer/page_timer.dart
@@ -1,5 +1,7 @@
+import 'package:dropdown_search/dropdown_search.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_i_see_u/modules/timer/controller_timer.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 class TimerPage extends GetView<TimerController> {
@@ -8,7 +10,51 @@ class TimerPage extends GetView<TimerController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      body: SafeArea(
+        child: SizedBox(
+          // height: Get.height,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              _buildDropdown(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 
+  Widget _buildDropdown() {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        SizedBox(
+          width: 160.w,
+          child: DropdownSearch<String>(
+            popupProps: PopupProps.menu(
+              showSelectedItems: true,
+              disabledItemFn: (String s) => s.startsWith('I'),
+            ),
+            items: ["Brazil", "Italia (Disabled)", "Tunisia", 'Canada'],
+            dropdownDecoratorProps: DropDownDecoratorProps(
+              dropdownSearchDecoration: InputDecoration(
+                hintText: "집중할 항목을 선택하세요.",
+              ),
+            ),
+            onChanged: print,
+            selectedItem: "Brazil",
+          ),
+        ),
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(Icons.add),
+          style: IconButton.styleFrom(
+            minimumSize: Size(24.w, 24.w),
+          ),
+        ),
+      ],
     );
   }
 }
